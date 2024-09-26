a55369d229f0e879e440efc5b63bd7761bd95db4
Yura Koshelenko <54513784+H4ide@users.noreply.github.com>
2024-09-26 19:39:38 +0200

    Update Main.hs

--- NFAtoDFAconverter/app/Main.hs
+++ NFAtoDFAconverter/app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import System.Environment (getArgs)
+import System.Environment (getArgs) -- get command-line arguments
 import NFAParser (readNFAFromFile)
 import NFAtoDFA (nfaToDfa)
 import DFA (printDfa, writeDfaToFile)
@@ -10,11 +10,12 @@ main = do
     args <- getArgs
     case args of
         [fileName] -> do
-            result <- readNFAFromFile fileName
+            result <- readNFAFromFile fileName                   -- if exactly one argument (the filename) was provided
             case result of
-                Right nfa -> do
+                Right nfa -> do -- if we get success in pasting NFA from file
                     let dfa = nfaToDfa nfa
                     printDfa dfa
                     writeDfaToFile fileName dfa
                 Left err -> putStrLn err
-        _ -> putStrLn "Error: You did not provide the filename."
+        _ -> putStrLn "Error: You did not provide the filename." -- Error otherwise
+
